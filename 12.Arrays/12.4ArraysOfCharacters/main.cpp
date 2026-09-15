ac4d7debcfc719331f981433f5814c77c73a5cf6
ismailfaruk <ismailfaruk.17@gmail.com>
2026-09-15 13:34:57 +0600

    match with tutorial

--- 12.Arrays/12.4ArraysOfCharacters/main.cpp
+++ 12.Arrays/12.4ArraysOfCharacters/main.cpp
@@ -4,7 +4,7 @@
 int main(){
 
 	//Declare array
-    char message [6]  {"HelLo"};
+    char message [] {'H','e','l','l','o'};
     //int data[5] {1,2,3,3,3};
 
     //std::cout << "data : " << data << std::endl;
@@ -20,7 +20,7 @@ int main(){
    
 
     
-   /*
+   
     //Change characters in our array
     message[1] = 'a';
 
@@ -30,26 +30,26 @@ int main(){
         std::cout << c ;
     }
     std::cout << std::endl;
-    */
+   
 
    
    
     // Will probably print garbage after your char array
-    /*
+    
     std::cout << "message : " << message << std::endl;
-    */
+   
    
 
  
     //If a character array is null terminated, it's called as C-String
-    /*
+    
     char message1 [] {'H','e','l','l','o','\0'};
     std::cout << "message1 : " << message1 << std::endl;
     std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
-    */
+   
 
   
-   /*
+   
     char message2 [6] {'H','e','l','l','o'};
     std::cout << "message2 : " << message2 << std::endl;
     std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
@@ -58,16 +58,16 @@ int main(){
 											//as there is not null character
     std::cout << "message3 : " << message3 << std::endl;
     std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl;
-    */
+   
 
     
 
     //String literal
-    /*
+    
     char message4 [] {"Hello"};
     std::cout << "message4 : " << message4 << std::endl;
     std::cout << "sizeof(message4) : " << sizeof(message4) << std::endl;
-    */
+   
 
 
 
